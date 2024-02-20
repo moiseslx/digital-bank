a1ee72304da41b7d13b2ded0e76d2b6ba9d58dfc
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-02-20 15:56:47 -0400

    Commit inicial

---
+++ src/main/java/br/com/cdb/digitalbank/DigitalBankEduc360Application.java
@@ -0,0 +1,13 @@
+package br.com.cdb.digitalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankEduc360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankEduc360Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cdb/digitalbank/DigitalBankEduc360ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdb.digitalbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankEduc360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
